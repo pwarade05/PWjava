2c6af5f29593fcd7c5c3c843070b7423cb6cebda
pwarade05 <pwarade05@gmail.com>
2023-02-07 09:02:47 +0530

    first commit

---
+++ Addition.java
@@ -0,0 +1,27 @@
+public class Addition {
+    public static void main(String[] args) {
+        System.out.println("Addition of Three number");
+        int a=20;
+        int b=10;
+        int c=40;
+        int Sum= a+b+c;
+
+        System.out.println("Answer is:");
+        System.out.println(Sum);  
+
+        System.out.println("---------------------------------");
+    
+             //calculate cgpa of class students.
+            // Marks of three subjecrts
+                        {
+                             float English=57;
+                             float Marathi=88;
+                             float Maths=45;
+                             float sum= English+Marathi+Maths;
+                            
+                             System.out.println("Total of 3 subjects is: "+sum);
+                              System.out.println("cgpa of 3 subjects is:"+sum/30);
+                            }
+                        }
+                    }
+
